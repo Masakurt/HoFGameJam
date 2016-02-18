cb6ab60344be464772bbb3ebcd30eb1db707da01
MasaKurt <mr.masakurt@gmail.com>
2016-02-17 21:22:00 -0500

    Added sounds

--- HoFGameJam/Assets/FIshScript.cs
+++ HoFGameJam/Assets/FIshScript.cs
@@ -4,17 +4,27 @@ using System.Collections;
 public class FIshScript : MonoBehaviour {
 
 	// Use this for initialization
+	public uint myIndex = 0;
 	void Start () {
-		Vector3 temp;
-		temp.x = Random.Range (-20, 20);
-		float yrange = Mathf.Sqrt (400 - temp.x * temp.x);
-		temp.y = Random.Range (-yrange, yrange);
-		temp.z = Mathf.Sqrt (400 - temp.x * temp.x - temp.y * temp.y) + -10;
-		transform.position = temp;
+		for (;;) {
+			Vector3 temp;
+			temp.x = Random.Range (-19, 19);
+			float yrange = Mathf.Sqrt (361 - temp.x * temp.x);
+			temp.y = Random.Range (-yrange, yrange);
+			temp.z = Mathf.Sqrt (361 - temp.x * temp.x - temp.y * temp.y) + -10;
+			transform.position = temp;
+			if (!(GeometryUtility.TestPlanesAABB (GeometryUtility.CalculateFrustumPlanes (Camera.main), GetComponent<Collider> ().bounds))) {
+				break;
+			}
+		}
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
+	}
+	void OnDestroy(){
+		GameObject.Find ("Main Camera").GetComponent<Reset> ().ChangeSound (myIndex);
+		//BroadcastMessage ("ChangeSound", myIndex);
 	}
 }
--- HoFGameJam/Assets/Reset.cs
+++ HoFGameJam/Assets/Reset.cs
@@ -4,13 +4,42 @@ using System.Collections;
 public class Reset : MonoBehaviour {
 
 	// Use this for initialization
-	public Mesh[] meshes;
+	public GameObject[] objects;
+	public string Name;
+	public string Details;
+	public GameObject target;
+	public bool found = false;
+	public AudioSource[] sounds;
 	void Start () {
-	
 	}
 	
 	// Update is called once per frame
-	void Update () {
-	
+	void FixedUpdate () {
+		if (Input.GetButtonDown("Fire1") && found) {
+			found = false;
+			int index = Random.Range(0, objects.Length);
+			DestroyImmediate(target);
+			target = GameObject.Instantiate(objects[index]);
+			GetComponent<Controls>().enabled = true;
+		}
+		if (target) {
+			if (GeometryUtility.TestPlanesAABB (GeometryUtility.CalculateFrustumPlanes (Camera.main), target.GetComponent<Collider> ().bounds)) {
+				found = true;				
+				Quaternion rotation = Quaternion.LookRotation(target.transform.position - transform.position);
+				transform.rotation = Quaternion.Slerp(transform.rotation, rotation, Time.deltaTime);
+				GetComponent<Controls> ().enabled = false;
+			}
+			else {
+				found = false;
+			}
+		}
+	}
+	public void ChangeSound(uint _index)	{
+		if (_index > sounds.Length || _index == 0)
+			return;
+		for (int i = 1; i < sounds.Length; i++) {
+			sounds[i].mute = true;
+		}
+		sounds [_index].mute = false;
 	}
 }
